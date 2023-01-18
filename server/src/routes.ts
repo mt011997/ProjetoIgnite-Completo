53f8bec068f96218e62d30be7830c41ed6fe2b75
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-01-18 05:59:08 -0300

    feat: Adiciona rota de criar habito e listar habitos completos/ possíveis, cria relações entre entidades e cria seed para o banco de dados

---
+++ server/src/routes.ts
@@ -0,0 +1,75 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "./lib/prisma";
+import { z } from "zod";
+import dayjs from "dayjs";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    // startOf = Zera a hora
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return { week_day: weekDay };
+          }),
+        },
+      },
+    });
+  });
+
+  app.get("/day", async (request) => {
+    //coerce = converte em string
+    const getDayParans = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParans.parse(request.query);
+
+    const parseDate = dayjs(date).startOf("day");
+    //pega um dia da semana em forma de numero 0-6
+    const weekDay = parseDate.get("day");
+
+    //lte = menor ou igual
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
+}
